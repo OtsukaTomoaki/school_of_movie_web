4de6c010ac8cd14201a680f34a2c430f89fe7068
OtsukaTomoaki <ootsuka.ootsuka.ootsuka.26@gmail.com>
2023-01-17 00:26:21 +0900

    websocketの実装

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,52 @@
 <template>
-  <hello-world />
+  <div id="app">
+    <h2>Vue.js WebSocket Tutorial</h2>
+    <button v-on:click="sendMessage('hello')">Send Message</button>
+  </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
 
 // Components
-import HelloWorld from '../components/HelloWorld.vue'
+import ActionCable from 'actioncable'
 
+const cable = ActionCable.createConsumer('ws://localhost:3000/cable')
+const chatChannel = cable.subscriptions.create(
+  {
+    channel: 'RoomChannel',
+    room: 'チャットルーム'
+  },
+  {
+    received (data) {
+      console.log(data.message)
+    }
+  }
+)
 export default defineComponent({
   name: 'HomeView',
 
   components: {
-    HelloWorld
+  },
+  data: function () {
+    return {
+      connection: null,
+      messages: [],
+      chatChannel: null
+    }
+  },
+  methods: {
+    sendMessage: function (message: string) {
+      chatChannel.perform('speak', {
+        message: message
+      })
+    },
+    received: function (message: string) {
+      console.log(message)
+    }
+  },
+  created: function () {
+    this.chatChannel = chatChannel
   }
 })
 </script>

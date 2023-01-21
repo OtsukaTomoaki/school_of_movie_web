cd007a6f431c54e719dc812407f355e978084566
OtsukaTomoaki <ootsuka.ootsuka.ootsuka.26@gmail.com>
2023-01-21 14:28:58 +0900

    チャット画面をコンポーネント化

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,32 +1,19 @@
 <template>
   <div id="app">
     <h2>Vue.js WebSocket Tutorial</h2>
-    <button v-on:click="sendMessage('hello')">Send Message</button>
+    <SpeakForm></SpeakForm>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
+import SpeakForm from '@/components/SpeakForm.vue'
 
-// Components
-import ActionCable from 'actioncable'
-
-const cable = ActionCable.createConsumer('ws://localhost:3000/cable')
-const chatChannel = cable.subscriptions.create(
-  {
-    channel: 'RoomChannel',
-    room: 'チャットルーム'
-  },
-  {
-    received (data) {
-      console.log(data.message)
-    }
-  }
-)
 export default defineComponent({
   name: 'HomeView',
 
   components: {
+    SpeakForm
   },
   data: function () {
     return {
@@ -34,19 +21,6 @@ export default defineComponent({
       messages: [],
       chatChannel: null
     }
-  },
-  methods: {
-    sendMessage: function (message: string) {
-      chatChannel.perform('speak', {
-        message: message
-      })
-    },
-    received: function (message: string) {
-      console.log(message)
-    }
-  },
-  created: function () {
-    this.chatChannel = chatChannel
   }
 })
 </script>

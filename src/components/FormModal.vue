8a6f7ecb3cab5d2ef2a63a370363a2ab7f21d6dc
OtsukaTomoaki <ootsuka.ootsuka.ootsuka.26@gmail.com>
2023-02-19 19:16:22 +0900

    モーダルの追加

---
+++ src/components/FormModal.vue
@@ -0,0 +1,68 @@
+<template>
+  <teleport to="body">
+    <div class="modal" v-show="props.show" @click="onclick"></div>
+    <div class="modal-content" v-show="props.show">
+      <slot></slot>
+    </div>
+  </teleport>
+</template>
+
+<script lang="ts">
+import { defineComponent, defineProps, defineEmits, onMounted, ref } from 'vue'
+import { Vue } from 'vue-class-component'
+
+export default defineComponent({
+  name: 'FromModal',
+  data: function () {
+    return {
+    }
+  }
+})
+</script>
+
+<script setup lang="ts">
+const props = defineProps({
+  show: Boolean
+})
+const show = ref(props.show)
+const emits = defineEmits(['close'])
+
+const onclick = function (this: Vue) {
+  console.log('clicked!!')
+  show.value = false
+  emits('close')
+}
+</script>
+
+<style scoped>
+.modal {
+  position: fixed;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  background-color: rgba(0, 0, 0, .5);
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+
+.modal-content {
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  background-color: rgba(0, 0, 0, .5);
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+  background-color: white;
+  width: 600px;
+  height: auto;
+  border-radius: 20px;
+  padding: 20px;
+}
+</style>

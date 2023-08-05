8943ee8a280e53dd89120de2fdfe0efcf8e877be
OtsukaTomoaki <ootsuka.ootsuka.ootsuka.26@gmail.com>
2023-08-05 19:51:36 +0900

    オリジナルのチェックボックスの作成

---
+++ src/components/atoms/CustomCheckBox.vue
@@ -0,0 +1,86 @@
+<template>
+  <div class="custom-checkbox-container">
+    <input type="checkbox" :id="id" :checked="checked" @change="emitChecked" :value="id">
+    <label :for="id" class="checkbox-label">
+      <div v-if="checked">
+        <vue-feather type="check" class="check-icon"></vue-feather>
+      </div>
+    </label>
+    <div v-if="text" class="checkbox-text">
+      {{ text }}
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+
+defineProps({
+  id: {
+    type: String,
+    required: true
+  },
+  checked: {
+    type: Boolean,
+    required: true
+  },
+  text : {
+    type: String,
+    required: false
+  }
+})
+
+const emit = defineEmits(['change', 'input'])
+
+const emitChecked = (e: Event) => {
+  console.log('emitChecked', (e.target as HTMLInputElement).checked)
+  emit('change', (e.target as HTMLInputElement).checked)
+  emit('input', (e.target as HTMLInputElement).checked)
+}
+</script>
+
+<style scoped>
+.custom-checkbox-container {
+  display: flex;
+  justify-content: left;
+  align-items: left;
+}
+.custom-checkbox-container .checkbox-label {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 15px;
+  height: 15px;
+  border: 1px solid #2c3e50;
+  border-radius: 3px;
+  margin-right: 5px;
+}
+.custom-checkbox-container .checkbox-label:hover {
+  background-color: #F0F0F0;
+  cursor: pointer;
+}
+
+.check-icon {
+  color: #2c3e50;
+  animation-name: fadein;
+  animation-duration: 1s;
+}
+
+.checkbox-text {
+  margin-left: 1px;
+  color: #2c3e50;
+}
+.custom-checkbox-container input[type="checkbox"] {
+  display: none;
+}
+
+@keyframes fadein{
+  from {
+    opacity: 0;
+    transform: translateX(3px);
+  }
+  to {
+    opacity: 1;
+    transform: translateX(0px)
+  }
+}
+</style>

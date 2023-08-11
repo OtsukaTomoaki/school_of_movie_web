b7958a3bb55502133d46469be4ecd6a324f0d648
OtsukaTomoaki <ootsuka.ootsuka.ootsuka.26@gmail.com>
2023-08-11 17:26:39 +0900

    profileの表示

---
+++ src/components/organisms/ProfileCard.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="profile-card">
+    <div class="profile-wrapper">
+
+      <div class="profile-card-avatar">
+        <img class="profile-avatar-image" src="http://localhost:3000/api/v1/users/download_avatar_image" />
+      </div>
+      <div class="profile-content" v-if="profile">
+        <div class="profile-card-email">
+          {{ profile.email }}
+        </div>
+        <div class="profile-card-name">
+          {{ profile.name }}
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { onMounted, onBeforeMount, ref } from 'vue'
+import { useStore } from 'vuex'
+import { FetchProfile } from '@/apis/accounts'
+import { Profile as ProfileType } from '@/profileTypes'
+
+const profile = ref<ProfileType>(null)
+
+onMounted(async function () {
+  profile.value = await FetchProfile()
+})
+
+</script>

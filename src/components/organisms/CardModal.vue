e0b504ca526190835de664f6f5c565a73ddd786a
OtsukaTomoaki <ootsuka.ootsuka.ootsuka.26@gmail.com>
2023-08-11 16:32:06 +0900

    プロフィール表示用のカード作成

---
+++ src/components/organisms/CardModal.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="card-modal-container">
+    <slot></slot>
+  </div>
+  <div class="card-modal-shadow" @click="onclickBackGround"></div>
+
+</template>
+
+<script setup lang="ts">
+const emits = defineEmits(['update:show'])
+
+const onclickBackGround = function () {
+  emits('update:show', false)
+}
+
+</script>
+
+<style scoped>
+.card-modal-container {
+  --element-width: 400px;
+
+  position: absolute;
+  display: block;
+  width: var(--element-width);
+  margin-left: calc(100% - var(--element-width));
+  height: auto;
+  background-color: white;
+  z-index: 100;
+  border-radius: 1%;
+
+  color: #2c3e50;
+}
+.card-modal-shadow {
+  position: fixed;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  background-color: rgba(0, 0, 0, .5);
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  z-index: 99;
+}
+</style>

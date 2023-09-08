1129e5e89f8f4157c10afc4a1970a86875aa8875
OtsukaTomoaki <ootsuka.ootsuka.ootsuka.26@gmail.com>
2023-09-08 17:39:53 +0900

    id, passの入力IFを削除

--- src/views/SignInView.vue
+++ src/views/SignInView.vue
@@ -1,12 +1,8 @@
 <template>
-    <div class="signin">
-        <IdPassForm />
-    </div>
     <SigninGoogle />
 </template>
 
 <script setup lang="ts">
-import IdPassForm from '@/components/IdPassForm.vue'
 import SigninGoogle from '@/components/molecules/SigninGoogle.vue'
 
 </script>

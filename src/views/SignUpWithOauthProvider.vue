47c12488b050af3c76f37bbfcc89c1ca06af7be1
OtsukaTomoaki <ootsuka.ootsuka.ootsuka.26@gmail.com>
2023-10-30 03:20:07 +0900

    サインアップ後にログインする

--- src/views/SignUpWithOauthProvider.vue
+++ src/views/SignUpWithOauthProvider.vue
@@ -32,11 +32,8 @@ export default {
     const image = signUpState.image
 
     const onclick:(payload: MouseEvent) => void = async (): Promise<void> => {
-      const result = await SignUpWithSocialAccounts(email, onetimeToken)
-
-      store.commit(UPDATE_AUTHORIZATION_TOKEN, result.authorizationToken)
-      store.commit(UPDATE_REMEMBER_TOKEN, result.rememberToken)
-      location.href = '/'
+      await SignUpWithSocialAccounts(email, onetimeToken)
+      window.location.href = `${process.env.VUE_APP_ROOT_API}/auth/google_oauth2`
     }
 
     return {
